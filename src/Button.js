ef2285696c52ee04a0b75f90e6f7ba081b0bec84
koehr <n@koehr.in>
2017-05-12 01:47:39 +0200

    button width

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,12 @@
 export default {
   functional: true,
   name: 'button',
-  props: ['label'],
+  props: ['label', 'width'],
   render (h, ctx) {
+    const width = ctx.props.width || 200
     return h('g', {'class':'button'}, [
-      h('rect', { attrs: {x:0, y:0, width:200, height:30, rx:15, ry:15} }),
-      h('text', { attrs: {x:100, y:22} }, [ctx.props.label])
+      h('rect', { attrs: {x:0, y:0, width, height:30, rx:15, ry:15} }),
+      h('text', { attrs: {x:width / 2, y:22} }, [ctx.props.label])
     ])
   }
 }
