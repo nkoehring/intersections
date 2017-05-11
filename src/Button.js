6a30afc67d9a56748694ac26147ed23e9df45923
koehr <n@koehr.in>
2017-05-12 00:33:45 +0200

    purely functional buttons

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,11 @@
 export default {
+  functional: true,
   name: 'button',
   props: ['label'],
-  render (h) {
+  render (h, ctx) {
     return h('g', {'class':'button'}, [
       h('rect', { attrs: {x:0, y:0, width:200, height:30, rx:15, ry:15} }),
-      h('text', { attrs: {x:100, y:22} }, [this.label])
+      h('text', { attrs: {x:100, y:22} }, [ctx.props.label])
     ])
   }
 }
